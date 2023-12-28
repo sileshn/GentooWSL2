sileshn/GentooWSL

Changes for Gentoo

Makefile
@@ -1,18 +1,17 @@
-OUT_ZIP=Alpine.zip
-LNCR_EXE=Alpine.exe
+OUT_ZIP=Gentoo.zip
+LNCR_EXE=Gentoo.exe
 
 DLR=curl
 DLR_FLAGS=-L
-BASE_URL=http://dl-cdn.alpinelinux.org/alpine/v3.11/releases/x86_64/alpine-minirootfs-3.11.5-x86_64.tar.gz
-LNCR_ZIP_URL=https://github.com/yuk7/wsldl/releases/download/20040300/icons.zip
-LNCR_ZIP_EXE=Alpine.exe
+LNCR_ZIP_URL=https://gitlab.com/sileshn/wsldl/uploads/3ea58028ae3839ffa6ab53cdefbcded5/icons.zip
+LNCR_ZIP_EXE=Gentoo.exe
 
 all: $(OUT_ZIP)
 
 zip: $(OUT_ZIP)
 $(OUT_ZIP): ziproot
 	@echo -e '\e[1;31mBuilding $(OUT_ZIP)\e[m'
-	cd ziproot; bsdtar -a -cf ../$(OUT_ZIP) *
+	cd ziproot; zip ../$(OUT_ZIP) *
 
 ziproot: Launcher.exe rootfs.tar.gz
 	@echo -e '\e[1;31mBuilding ziproot...\e[m'
@@ -23,7 +22,7 @@ ziproot: Launcher.exe rootfs.tar.gz
 exe: Launcher.exe
 Launcher.exe: icons.zip
 	@echo -e '\e[1;31mExtracting Launcher.exe...\e[m'
-	bsdtar -xvf icons.zip $(LNCR_ZIP_EXE)
+	unzip icons.zip $(LNCR_ZIP_EXE)
 	mv $(LNCR_ZIP_EXE) Launcher.exe
 
 icons.zip:
@@ -35,21 +34,18 @@ rootfs.tar.gz: rootfs
 	cd rootfs; sudo tar -zcpf ../rootfs.tar.gz `sudo ls`
 	sudo chown `id -un` rootfs.tar.gz
 
-rootfs: base.tar.gz profile
+rootfs: base.tar
 	@echo -e '\e[1;31mBuilding rootfs...\e[m'
 	mkdir rootfs
-	sudo tar -zxpf base.tar.gz -C rootfs
-	sudo cp -f /etc/resolv.conf rootfs/etc/resolv.conf
-	sudo cp -f profile rootfs/etc/profile
-	sudo chroot rootfs /sbin/apk update
-	sudo chroot rootfs /sbin/apk add bash
-	echo "# This file was automatically generated by WSL. To stop automatic generation of this file, remove this line." | sudo tee rootfs/etc/resolv.conf
-	sudo rm -rf `sudo find rootfs/var/cache/apk/ -type f`
+	sudo tar -xpf base.tar -C rootfs
+	@echo "# This file was automatically generated by WSL. To stop automatic generation of this file, remove this line." | sudo tee rootfs/etc/resolv.conf > /dev/null
 	sudo chmod +x rootfs
 
-base.tar.gz:
-	@echo -e '\e[1;31mDownloading base.tar.gz...\e[m'
-	$(DLR) $(DLR_FLAGS) $(BASE_URL) -o base.tar.gz
+base.tar:
+	@echo -e '\e[1;31mExporting base.tar using docker...\e[m'
+	docker run --name gentoowsl gentoo/stage3:latest /bin/bash -c "emerge --sync; emerge --update --deep --with-bdeps=y @world; emerge --update --deep --with-bdeps=y @system; emerge sudo"
+	docker export --output=base.tar gentoowsl
+	docker rm -f gentoowsl
 
 clean:
 	@echo -e '\e[1;31mCleaning files...\e[m'
@@ -59,4 +55,5 @@ clean:
 	-rm icons.zip
 	-rm rootfs.tar.gz
 	-sudo rm -r rootfs
-	-rm base.tar.gz
+	-rm base.tar
+	-docker rmi gentoo/stage3:latest -f

sileshn/GentooWSL2

Makefile
@@ -1,11 +1,10 @@
-OUT_ZIP=Alpine.zip
-LNCR_EXE=Alpine.exe
+OUT_ZIP=GentooWSL2.zip
+LNCR_EXE=Gentoo.exe
 
 DLR=curl
 DLR_FLAGS=-L
-BASE_URL=http://dl-cdn.alpinelinux.org/alpine/v3.11/releases/x86_64/alpine-minirootfs-3.11.5-x86_64.tar.gz
-LNCR_ZIP_URL=https://github.com/yuk7/wsldl/releases/download/20040300/icons.zip
-LNCR_ZIP_EXE=Alpine.exe
+LNCR_ZIP_URL=https://github.com/yuk7/wsldl/releases/download/23072600/icons.zip
+LNCR_ZIP_EXE=Gentoo.exe
 
 all: $(OUT_ZIP)
 
@@ -35,21 +34,20 @@ rootfs.tar.gz: rootfs
 	cd rootfs; sudo tar -zcpf ../rootfs.tar.gz `sudo ls`
 	sudo chown `id -un` rootfs.tar.gz
 
-rootfs: base.tar.gz profile
+rootfs: base.tar
 	@echo -e '\e[1;31mBuilding rootfs...\e[m'
 	mkdir rootfs
-	sudo tar -zxpf base.tar.gz -C rootfs
-	sudo cp -f /etc/resolv.conf rootfs/etc/resolv.conf
-	sudo cp -f profile rootfs/etc/profile
-	sudo chroot rootfs /sbin/apk update
-	sudo chroot rootfs /sbin/apk add bash
-	echo "# This file was automatically generated by WSL. To stop automatic generation of this file, remove this line." | sudo tee rootfs/etc/resolv.conf
-	sudo rm -rf `sudo find rootfs/var/cache/apk/ -type f`
+	sudo bsdtar -zxpf base.tar -C rootfs
+	@echo "# This file was automatically generated by WSL. To stop automatic generation of this file, remove this line." | sudo tee rootfs/etc/resolv.conf > /dev/null
+	sudo cp wsl.conf rootfs/etc/wsl.conf
+	sudo cp bash_profile rootfs/root/.bash_profile
 	sudo chmod +x rootfs
 
-base.tar.gz:
-	@echo -e '\e[1;31mDownloading base.tar.gz...\e[m'
-	$(DLR) $(DLR_FLAGS) $(BASE_URL) -o base.tar.gz
+base.tar:
+	@echo -e '\e[1;31mExporting base.tar using docker...\e[m'
+	docker run --name gentoowsl gentoo/stage3:latest /bin/bash -c "emerge-webrsync; emerge -vDuN system; emerge -vDuN world; echo '=games-misc/lolcat-100.0.1-r2 ~amd64' | tee -a /etc/portage/package.accept_keywords/lolcat && echo '=games-misc/lolcat-100.0.1-r3 ~amd64' | tee -a /etc/portage/package.accept_keywords/lolcat && echo '=dev-ruby/optimist-3.1.0 ~amd64' | tee -a /etc/portage/package.accept_keywords/lolcat && echo '=dev-ruby/paint-2.3.0-r1 ~amd64' | tee -a /etc/portage/package.accept_keywords/lolcat; emerge figlet lolcat neofetch procps sudo"
+	docker export --output=base.tar gentoowsl
+	docker rm -f gentoowsl
 
 clean:
 	@echo -e '\e[1;31mCleaning files...\e[m'
@@ -59,4 +57,5 @@ clean:
 	-rm icons.zip
 	-rm rootfs.tar.gz
 	-sudo rm -r rootfs
-	-rm base.tar.gz
+	-rm base.tar
+	-docker rmi gentoo/stage3:latest -f